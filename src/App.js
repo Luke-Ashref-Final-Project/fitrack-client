1264a1a6b631511281bcbb6d940df8d300d8cac9
Luke Chen <heylukechen@gmail.com>
2023-08-24 08:49:17 +0200

    create sign up form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import HomePage from "./pages/HomePage";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer.js";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
       <Footer />
     </div>
