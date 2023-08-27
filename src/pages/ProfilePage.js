7a7c34038be6b63dc9757170b5716576b8d70b8c
Achref LAABIDI <abidi.achref95@gmail.com>
2023-08-27 02:43:12 +0100

    non loggedin users cant access profilepage

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -27,7 +27,9 @@ const ProfilePage = () => {
       setError("Failed to change password."); 
     }
   };
-
+  if (!isLoggedIn) {
+    navigate("/")
+  }
 
   useEffect(() => {
     if (user && user.userType === "coach") {
