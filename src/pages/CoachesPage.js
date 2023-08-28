c9f04ed8f7320d415297fb6d4db9d40bb60c337a
Achref LAABIDI <abidi.achref95@gmail.com>
2023-08-28 14:31:33 +0100

    fixed a bug related to the coaches page

--- src/pages/CoachesPage.js
+++ src/pages/CoachesPage.js
@@ -8,28 +8,26 @@ import Nav from "../components/Nav";
 const CoachesPage = () => {
   const [theme, setTheme] = useState("cmyk");
   const [coachesList, setCoachesList] = useState([]);
-  const { user, isLoggedIn } = useContext(AuthContext);
+  const { user, isLoggedIn, isLoading } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  const getCoaches = async () => {
-    try {
-        if (user && user.userType === "client") {
-            const response = await authMethods.getCoaches();
-            console.log(response);
-            setCoachesList(response);
-        } else {
-            console.log("you're not a client")
-            navigate("/")
-        }
-
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
-    getCoaches();
-  }, []);
+    if (user && user.userType === "client") {
+      const getCoaches = async () => {
+        try {
+          const response = await authMethods.getCoaches();
+          console.log(response);
+          setCoachesList(response);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      getCoaches();
+    } else if (user && user.userType !== "client") {
+      console.log("you're not a client");
+      navigate("/");
+    }
+  }, [user, navigate]);
 
   useEffect(() => {
     if (user && user.userType === "client") {
@@ -37,20 +35,24 @@ const CoachesPage = () => {
     }
   }, [user]);
 
+  if (isLoading) {
+    return <span className="loading loading-spinner text-error"></span>;
+  }
+
   return (
     isLoggedIn && (
-        <div>
-            <Nav />
-          {coachesList?.length > 0 ? (
-            coachesList?.map((coach) => (
-              <h1 key={coach._id}>{coach?.username}</h1>
-            ))
-          ) : (
-            <p>No coaches available.</p>
-          )}
-        </div>
-      )
-    );
+      <div>
+        <Nav />
+        {coachesList?.length > 0 ? (
+          coachesList?.map((coach) => (
+            <h1 key={coach._id}>{coach?.username}</h1>
+          ))
+        ) : (
+          <p>No coaches available.</p>
+        )}
+      </div>
+    )
+  );
 };
 
 export default CoachesPage;
