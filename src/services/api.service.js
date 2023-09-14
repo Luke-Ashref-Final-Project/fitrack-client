0442f095f5d87beb4abc891becdfedd6a4432e4f
Luke Chen <heylukechen@gmail.com>
2023-09-14 21:29:14 +0200

    changes

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -5,7 +5,7 @@ const apiExternal = axios.create({
 });
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL 
+  baseURL:  "http://localhost:5005" || process.env.REACT_APP_API_URL 
 });
 
 const specifiedOptions = {
